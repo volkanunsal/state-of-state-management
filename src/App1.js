35b4e3eb93ad2711ed16e04dcfbc6c796aaddbc5
Volkan Unsal <spocksplanet@gmail.com>
2021-02-06 18:25:33 -0500

    init commit

---
+++ src/App1.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react'
+import { proxy, useProxy } from 'valtio'
+
+const state = proxy({
+  count: 0,
+  nested: { ticks: 0 },
+})
+
+function Counter() {
+  const snapshot = useProxy(state)
+  useEffect(() => {
+    console.log('render Counter')
+  })
+  return <h1>{snapshot.count}</h1>
+}
+
+function Controls() {
+  const incCounter = () => {
+    state.count++
+  }
+  const incTicks = () => {
+    state.nested.ticks++
+  }
+  useEffect(() => {
+    console.log('render Controls')
+  })
+  return (
+    <div>
+      <button onClick={incCounter}>count</button>
+      <button onClick={incTicks}>tick</button>
+    </div>
+  )
+}
+
+function Ticks() {
+  const snapshot = useProxy(state)
+  useEffect(() => {
+    console.log('render Ticks')
+  })
+  return <h1>{snapshot.nested.ticks}</h1>
+}
+
+export default function App() {
+  return (
+    <>
+      <Counter />
+      <Ticks />
+      <Controls />
+    </>
+  )
+}
---
+++ src/App5.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from 'react'
+import { connect, Provider, useStore } from 'react-redux'
+import { createStore } from 'redux'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INC':
+      return { ...state, count: state.count + 1 }
+    case 'TICK':
+      return { ...state, tick: state.tick + 1 }
+  }
+  return state
+}
+
+const store = createStore(reducer, { tick: 0, count: 0 })
+
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+    tick: state.tick,
+  }
+}
+
+const Counter = connect(mapStateToProps)((props) => {
+  useEffect(() => {
+    console.log('render Counter')
+  })
+  return <h1>{props.count}</h1>
+})
+
+const Ticker = connect(mapStateToProps)((props) => {
+  useEffect(() => {
+    console.log('render Ticker')
+  })
+  return <h1>{props.tick}</h1>
+})
+
+function Controls() {
+  const store2 = useStore()
+  const inc = () => store2.dispatch({ type: 'INC' })
+  const tick = () => store2.dispatch({ type: 'TICK' })
+  useEffect(() => {
+    console.log('render Controls')
+  })
+  return (
+    <>
+      <button onClick={inc}>count</button>
+      <button onClick={tick}>tick</button>
+    </>
+  )
+}
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Counter />
+      <Ticker />
+      <Controls />
+    </Provider>
+  )
+}
+
+export default App
